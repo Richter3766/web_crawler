2c0fa9d83b63c4730895586097ffa87c86cc328b
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-01-22 20:18:25 +0900

    feat: env 파일 설정
    크롤링 시 User agent 헤더 추가를 위한
    .env 파일 추가
    .env 적용을 위한 의존성 python-dotenv 추가

--- app/app.py
+++ app/app.py
@@ -1,9 +1,14 @@
+import os
+
 from flask import Flask
+from dotenv import load_dotenv
 
 from app.routes import health_check_bp
 
+load_dotenv()
 app = Flask(__name__)
 app.register_blueprint(health_check_bp)
+user_agent = os.getenv('USER_AGENT', 'default_user_agent')
 
 if __name__ == '__main__':
     app.run()

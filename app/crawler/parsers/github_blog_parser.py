8301588d71262558e8150053ad8e2b2b61f6cb10
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-01-27 15:21:03 +0900

    feat: 파서 함수 예외 처리
    파싱 시 오류 핸들링 추가.
    잘못된 형식이라면 무시하도록 만듦.

--- app/crawler/parsers/github_blog_parser.py
+++ app/crawler/parsers/github_blog_parser.py
@@ -1,6 +1,7 @@
 import hashlib
 
 from bs4 import BeautifulSoup
+from requests import Response
 
 from app.crawler.parsers import ContentParser
 from app.dto.crawled_dto import CrawledDto
@@ -10,16 +11,18 @@ def generate_hash(content):
     return hashlib.sha256(content.encode('utf-8')).hexdigest()
 
 class GithubBlogParser(ContentParser):
-    def parse(self, data):
+    def parse(self, data: Response):
         soup = BeautifulSoup(data.text, 'html.parser')
-
-        title = soup.select("body > header > div > div > div > div > div > h1")[0].text
-        contents = soup.select("body > main > div > div > ul.posts-list.list-unstyled")[0].text
+        try:
+            title = soup.select_one("body > header > div > div > div > div > div > h1").text
+            contents = soup.get_text(separator='\n', strip=True)
+            urls = [a['href'] for a in soup.find_all('a', href=True)]
+        except Exception as e:
+            print("parser 에러: ", data.url, e)
+            return None
 
         if not title or not contents:
             return None
-
-        urls = [a['href'] for a in soup.find_all('a', href=True)]
 
         hash_target = ' '.join(title + contents)
         hash_digest = generate_hash(hash_target)

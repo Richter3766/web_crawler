3657290bccac4e167cff84e77ba37b1c6920e8cb
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-01-27 15:18:18 +0900

    refactor: db 연결 개선
    클래스가 아니라 스레드에서 db 연결 세션을 관리하도록 변경함.

--- app/crawler/data_processor.py
+++ app/crawler/data_processor.py
@@ -1,6 +1,7 @@
 import queue
 
-from app import db_manager
+from sqlalchemy.orm import Session
+
 from app.database import CrawledIndex
 from app.dto.crawled_dto import CrawledDto
 
@@ -8,29 +9,28 @@ from app.dto.crawled_dto import CrawledDto
 class DataProcessor:
     def __init__(self):
         self.queue: queue.Queue[CrawledDto] = queue.Queue()
-        self.session = db_manager.get_session()
 
     def append_dto(self, dto: CrawledDto):
         self.queue.put(dto)
 
-    def get_non_duplicate_data(self):
+    def get_non_duplicate_data(self, session: Session):
         data: CrawledDto = self.queue.get()
-        result = self.session.query(CrawledIndex).filter(CrawledIndex.hash_value == data.hash_value).first()
+        result = session.query(CrawledIndex).filter(CrawledIndex.hash_value == data.hash_value).first()
         if result is None:
             return data
         return None
 
-    def add_data(self, data):
+    def add_data(self, session: Session, data):
         if isinstance(data, list):
-            self.session.add_all(data)
+            session.add_all(data)
         else:
-            self.session.add(data)
+            session.add(data)
 
-    def commit(self):
-        self.session.commit()
+    def commit(self, session: Session):
+        session.commit()
 
-    def rollback(self):
-        self.session.rollback()
+    def rollback(self, session: Session):
+        session.rollback()
 
-    def delete_data(self, data_id):
-        self.session.delete(data_id)
+    def delete_data(self, session: Session, data_id):
+        session.delete(data_id)

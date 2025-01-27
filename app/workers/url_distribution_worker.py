df3a4334132a80269b5dd2c0bc7ffcf502e0e59a
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-01-27 15:21:22 +0900

    feat: url 배분 스레드 함수 추가

---
+++ app/workers/url_distribution_worker.py
@@ -0,0 +1,14 @@
+import os
+from urllib.parse import urlparse
+
+from app.crawler.crawler import github_blog_crawler
+from app.crawler.url_selector import UrlSelector
+
+
+def url_distribution_worker(url_selector: UrlSelector):
+    while True:
+        url = url_selector.select_url()
+        print("url 선택: ", url)
+        prefix = urlparse(url).netloc
+        if prefix == os.getenv('HYEONSOO_BLOG_URL'):
+            github_blog_crawler.add_url(url)

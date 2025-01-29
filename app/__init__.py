b6d08d9e1bd4de0b5cd24f95342a9635b735244e
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-01-29 14:08:52 +0900

    feat: flask 실행 시 크롤링 스레드 실행

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,18 @@
-import os
-
+from dotenv import load_dotenv
 from flask import Flask
 
 from .database import *
 from .routes import *
-from dotenv import load_dotenv
+from .workers import *
 
 load_dotenv()
-
-db_url = os.getenv('DATABASE_URL')
-db_manager = DatabaseManager(db_url)
 
 def create_app():
     app = Flask(__name__)
 
     register_blueprint(app)
     create_db_file()
+    run_workers(3)
 
     return app
 
@@ -29,3 +26,9 @@ def create_db_file():
     db_manager.create_tables()
     create_url_table()
     create_queue_table()
+
+
+def run_workers(num_threads):
+    url_thread = create_threads(url_distribution_worker, num_threads, ())
+    crawling_threads = create_threads(crawling_worker, num_threads, (github_blog_crawler,))
+    db_threads = create_threads(db_worker, num_threads, ())

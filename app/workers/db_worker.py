3c953f435b1a4d8c5b3aa5aa7507d967f341e120
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-01-30 13:45:02 +0900

    refactor: url 필터링은 스레드 풀 활용
    효율적인 자원 운용을 위해
    db_worker에서 활용하는 url 필터링은
    스레드풀을 활용해 동작하도록 개선.

--- app/workers/db_worker.py
+++ app/workers/db_worker.py
@@ -18,29 +18,30 @@ def process_urls(crawled_dto):
 
 def db_worker():
     session = db_manager.get_session()
-    while worker_manager.working:
-        crawled_dto: CrawledDto = data_processor.get_non_duplicate_data(session)
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        while worker_manager.working:
+            crawled_dto: CrawledDto = data_processor.get_non_duplicate_data(session)
 
-        if crawled_dto is not None:
-            print("크롤링 데이터 저장 시작: ", crawled_dto.url)
-            crawled_index: CrawledIndex = crawled_dto.to_index()
-            try:
-                session.add(crawled_index)
-                session.commit()
+            if crawled_dto is not None:
+                print("크롤링 데이터 저장 시작: ", crawled_dto.url)
+                crawled_index: CrawledIndex = crawled_dto.to_index()
+                try:
+                    session.add(crawled_index)
+                    session.commit()
 
-                crawled_data = crawled_dto.to_data(crawled_index.id)
-                crawled_index.crawled_data.append(crawled_data)
+                    crawled_data = crawled_dto.to_data(crawled_index.id)
+                    crawled_index.crawled_data.append(crawled_data)
 
-                session.add(crawled_data)
-                session.commit()
-            except Exception as e:
-                print("에러 ", e)
-                session.rollback()
-                session.delete(crawled_index)
-                session.commit()
-                continue
+                    session.add(crawled_data)
+                    session.commit()
+                except Exception as e:
+                    print("에러 ", e)
+                    session.rollback()
+                    session.delete(crawled_index)
+                    session.commit()
+                    continue
 
-            print("크롤링 데이터 저장 종료: ", crawled_dto.url)
-            worker_manager.create_threads(process_urls, 1, (crawled_dto, ))
+                print("크롤링 데이터 저장 종료: ", crawled_dto.url)
+                executor.submit(process_urls, crawled_dto)
 
     session.close()

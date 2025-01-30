b73a085fa080909b47d54397ff05ab9b87f3bde8
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-01-30 11:57:46 +0900

    refactor: 스레드 종료 시 print 문 개선
    몇 번 스레드인지 더 명확하게 알 수 있도록 개선함.

--- app/workers/worker_manager.py
+++ app/workers/worker_manager.py
@@ -6,8 +6,9 @@ class WorkerManager:
         self.working = True
 
     def create_threads(self, worker_function, num_threads, args, name = "임시"):
-        for _ in range(num_threads):
-            thread = threading.Thread(target=worker_function, args=args, name=name)
+        for i in range(num_threads):
+            thread_name = name + " 스레드 " + str(i + 1)
+            thread = threading.Thread(target=worker_function, args=args, name=thread_name)
             self.threads.append(thread)
             thread.start()
 
@@ -16,6 +17,6 @@ class WorkerManager:
         print("===================== 스레드 종료 시작 =====================")
         for i in range(len(self.threads)):
             self.threads[i].join()
-            print(f"------------- {self.threads[i].name} 스레드 {i + 1} 종료 완료 -------------")
+            print(f"------------- {self.threads[i].name} 종료 완료 -------------")
 
         print("===================== 스레드 종료 완료 =====================")

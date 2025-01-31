5295ab0895dca7cec6395164d9a7a45ca0b1d33e
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2025-01-31 10:17:04 +0900

    feat: failed url 조회 API 추가

--- app/routes/web_crawler_route.py
+++ app/routes/web_crawler_route.py
@@ -1,11 +1,16 @@
 from flask import request, jsonify
 
 from app.routes import web_crawler_bp
-from app.services.web_crawler_service import add_url
+from app.services.web_crawler_service import add_url, get_failed_data_list
 
 
 @web_crawler_bp.route('/crawler', methods=['POST'])
 def post_crawler_url():
-    reqeust_body = request.get_json()
-    result = add_url(reqeust_body['url'])
+    requests_body = request.get_json()
+    result = add_url(requests_body['url'])
+    return jsonify(result), 200
+
+@web_crawler_bp.route('/crawler/failed', methods=['GET'])
+def get_failed_url():
+    result = get_failed_data_list()
     return jsonify(result), 200
--- app/services/web_crawler_service.py
+++ app/services/web_crawler_service.py
@@ -1,6 +1,12 @@
+from app import failed_url_collector
 from app.crawler import url_selector
 
 
 def add_url(url: str):
     url_selector.append_url(url)
     return {"message": "successfully add " + url}
+
+def get_failed_data_list():
+    failed_data = failed_url_collector.get_data()
+
+    return failed_data
